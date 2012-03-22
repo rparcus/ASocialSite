d56c1850c0aaa44b4d59578d3eed07ce1257a6dc
Andrea <Andrea@Andrea-PC.homenet.telecomitalia.it>
2012-03-22 12:05:35 +0100

    Esempio: PHP legge un file xml

---
+++ samples/test_page.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<p>CIAO MONDO</p>
+<!--PHP legge un file xml e ne stampa gli elementi, cercavo un modo per "embeddare" un file xml(con xsl) in un file php ma boh..-->
+<?php
+    $url="file_mod.xml";
+    $xml = simplexml_load_file($url); 
+    foreach($xml->post as $post){
+        echo $post->post_title . "<br/>";
+        echo $post->post_body . "<hr/>";
+    }
+?>
+
+</body>
+</html>
