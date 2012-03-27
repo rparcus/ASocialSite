3826c8fc19d2267e0ed830ea483ec0bcd6157de8
Robert Parcus <betoparcus@gmail.com>
2012-03-27 13:53:34 +0200

    helper.php - piccole aggiunte, prototipi di funzioni che serviranno in seguito

--- helper.php
+++ helper.php
@@ -7,11 +7,14 @@
  * 
  */
 
-//agrega i parametri da inviare as WSDL sotto l'element 'parameters'
+//agrega i parametri da inviare al WSDL come elementXML 'parameters'
 function paramWrapper ($parameters){
     return array('parameters' => $parameters);
 }
 
+/*  controlla che la combinazione unica nomeutente->password sia presente nel db
+ *  restituisce -1 con un errore o in caso di password sbagliata.
+ */
 function checkPassword($username, $password){
     $wsdl = "http://localhost:8080/ASocialServer/ASocialService?wsdl";
     $client = new SoapClient($wsdl, array('trace' => 1));
@@ -19,6 +22,20 @@ function checkPassword($username, $password){
     $params = array('username' =>$username,'password'=>$password);
     $tmp = $client->__soapCall($function, paramWrapper($params));
     return $tmp->return; 
+}
+
+function getUsername($userID){
+    
+    /*
+     * qui ci va una funziona che mette in input userID, e ottiene lo userName
+     */
+}
+
+function checkWichSessio($userID){
+    /*
+     * Prende in input userID e dice se quella sessione è attiva o meno
+     * 
+     */
 }
 
 
