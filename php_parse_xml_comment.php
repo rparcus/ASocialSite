219fa47dacf72344a02b4e53e7781ee23efd679a
quiuquio <betoparcus@gmail.com>
2012-11-29 17:58:13 +0100

    php_parse_xml_comment : Utilizzo di echo per il codice html interno al php. Modifica neccessaria per le tecniche di longpolling

--- php_parse_xml_comment.php
+++ php_parse_xml_comment.php
@@ -8,21 +8,24 @@ function getComment($pid){
             if($comm->post_id==$pid) {
                 $anchor="commentn".$comm->comment_id;
                 global $avatarFolder; //helper.php
-                echo '<a name="'.$anchor.'"></a>' 
-?>      
-<div class="comment_content">
-    <div class="comment_image_block">          
-    	<div class="comment_image">
-            <img src="<?php echo $avatarFolder.$comm->user_id.".jpeg"; ?>" width="40" height="40"/>
-        </div>
-    	<div class="comment_inner_content">
-        	<div>
-                    <div class="comment_author"><?php /*echo getUsername($comm->user_id);*/
-                        echo $comm->user_id;?></div>
-                    <div class="comment_date"><?php echo $comm->comment_date; ?></div>
-                </div>
-            <div class="comment_text"><?php echo URLify($comm->comment_body); ?></div>  
-        </div>
-    </div>
-</div>
-<?php }}} ?>
+                echo '<a name="'.$anchor.'"></a>'; 
+    
+echo "<div class=\"comment_content\">";
+    echo "<div class=\"comment_image_block\">";          
+    	echo "<div class=\"comment_image\">";
+            echo "<img src=\"".$avatarFolder.$comm->user_id.".jpeg\" width=\"40\" height=\"40\"/>";
+        echo "</div>";
+    	echo "<div class=\"comment_inner_content\">";
+        	echo "<div>";
+                    echo "<div class=\"comment_author\">"; /*echo getUsername($comm->user_id);*/
+                        echo $comm->user_id."</div>";
+                    echo "<div class=\"comment_date\">".$comm->comment_date."</div>";
+                echo "</div>";
+            echo "<div class=\"comment_text\">".URLify($comm->comment_body)."</div>";  
+        echo "</div>";
+    echo "</div>";
+echo "</div>";
+                        }
+                    }
+                } 
+?>
